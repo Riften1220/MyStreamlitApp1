9904c6cf8f3d441c29e2491071f13d852101eea0
Wispermill <riften@126.com>
2024-07-27 16:06:07 +0800

    20240728-702

--- Script.py
+++ Script.py
@@ -7,26 +7,46 @@ st.markdown('<style>p { font-size: 16px; }</style>', unsafe_allow_html=True)
  
 st.title(r"Welcome To Billy's English Lesson!")
 
-st.header('Question1')
-st.write('___ it is today!',unsafe_allow_html=True)
-options = ['A. What fine weather', 'B. What a fine weather' ,'C. How a fine weather' ,'D. How fine a weather']
-selection = st.radio('**Select an option:**', options)
+df = pd.read_excel(r'data.xlsx',dtype=object)
 
-if st.button('查看答案和解析',1,type='primary'):
-    if selection == options[0]:
-        st.write('Well Done, your answer is correct')
-    else:
-        st.write('what a pity! Let Billy tell you why.Because...')
+st.sidebar.title(r'题库选项')
+
+set_grade = st.sidebar.selectbox(
+    '选择题库年级',
+    ['全部','初一', '初二']
+)
+
+set_difficulty = st.sidebar.selectbox(
+    '选择难度等级',
+    ['全部','中等', '简单']
+)
+if set_grade!='全部':
+    df=df[df['grade']==set_grade]
+
+if set_difficulty!='全部':
+    df=df[df['difficultylevel']==set_difficulty]
+
+count = 1
+for i in df.index:
+    st.header('第'+str(count)+'题：')
+    count=count+1
+    st.write(df.loc[i,'stem'])
+    
+    options = ['A.'+df.loc[i,'choiceA'],'B.'+df.loc[i,'choiceB'],'C.'+df.loc[i,'choiceC'],'D.'+df.loc[i,'choiceD']]
+    selection = st.radio('**Select an option:**', options)
+
+    choice = options.index(selection)+1
+    right_answer = df.loc[i,'correctanswer']
+    is_correct = choice == right_answer
 
 
-
-st.header('Question2')
-st.write('Which is the way to the ______?')
-options2 = ['A. shoe factory', 'B. shoes factory', r"C. shoes' factory",r"D. shoe's factory"]
-selection2 = st.radio('Your Answer is:', options2)
-
-if st.button('查看答案和解析',2):
-    if selection2 == options2[1]:
-        st.write('Well Done, your answer is correct')
-    else:
-        st.write('what a pity! Let Billy tell you why.')
+    if st.button('查看答案和解析',count):
+        st.write(df.loc[i,'explanation_general'])
+        if choice==1:
+            st.write(df.loc[i,'explanationA'])
+        elif choice==2:
+            st.write(df.loc[i,'explanationB'])
+        elif choice==3:
+            st.write(df.loc[i,'explanationC'])
+        elif choice==4:
+            st.write(df.loc[i,'explanationD'])
